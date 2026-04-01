87f1c1f00eb5ee429e71f9b15fe7b4e4e3d44298
MasterWhis <puspok.akos@gmail.com>
2026-04-01 16:37:39 +0200

    Pressing an operator now evaluates the function, so this: 1-3*5+3 can be calculated using the operations totals as the num1 for the nex operation

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+const add = (num1, num2) => +num1 + +num2;
+const subtract = (num1, num2) => +num1 - +num2;
+const multiply = (num1, num2) => +num1 * +num2;
+const divide = (num1, num2) => +num1 / +num2;
 let num1 = '';
 let num2 = '';
 let operator = '';
-const operate = (num1, num2, operator) => {
-    switch(operator) {
-        case '+':
-            add(num1, num2);
-            break;
-        case '-':
-            subtract(num1, num2);
-            break;
-        case '*':
-            multiply(num1, num2);
-            break;
-        case '/':
-            divide(num1, num2);
-            break;
-    }
-}
+
+function operate (num1, operator, num2){
+    if(operator === '+') {return add(num1, num2)};
+    if(operator === '-') {return subtract(num1, num2)};
+    if(operator === '*') {return multiply(num1, num2)};
+    if(operator === '/') {return divide(num1, num2)};
+};
+
 const operationButtons = document.querySelectorAll('.op');
 const numButtons = document.querySelectorAll('.num');
 
@@ -35,11 +27,27 @@ numButtons.forEach(item => item.addEventListener('click', (e) => {
 }));
 
 operationButtons.forEach(item => item.addEventListener('click', (e) => {
-    operator = item.textContent;
-    console.table(operator);
+    if (operator != '') {
+        doTheOperation()
+        operator = item.textContent
+    } else {
+    operator.textContent = '';
+    operator = item.textContent
+    console.log(operator);
+    };
+    
 }));
 
 const display = document.querySelector('.disp');
-function refreshDisp(num1, operator, num2){
+function refreshDisp(num1, operator = '', num2 = ''){
     display.textContent = `${num1} ${operator} ${num2}`;
 };
+
+const equal = document.querySelector('.eq');
+equal.addEventListener('click', window.doTheOperation = function() {
+    let total = operate(num1, operator, num2);
+    num1.textContent = '';
+    num2 = '';
+    num1 = total;
+    refreshDisp(total);
+});
