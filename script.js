78b9714f5908a17382bdab314aea4e38e5ab3629
MasterWhis <puspok.akos@gmail.com>
2026-04-01 15:16:59 +0200

    This is a new repo for my original calculator repo It got corrupted so I decided to put things here from now on. Sadly I lost all my other commits.
    The num1 variable got setup so that it holds any number being pressed before the operator.

---
+++ script.js
@@ -0,0 +1,33 @@
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+let num1 = '';
+let num2 = '';
+let operator = ''
+const operate = (num1, num2, operator) => {
+    switch(operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
+}
+const operationButtons = document.querySelectorAll('.op');
+const numButtons = document.querySelectorAll('.num');
+
+//Get the num1 value when buttons are pressed before operator
+numButtons.forEach(item => item.addEventListener('click', (e) => {
+    console.log(item.textContent);
+    num1 += item.textContent;
+    console.log(num1);
+    
+}));
